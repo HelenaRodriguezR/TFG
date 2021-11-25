c9c732ba4ef57a5cd1c8fbf55ce3ffecd3b77dee
HelenaRodriguezR <helena.rodriguezr@hotmail.com>
2021-11-25 03:24:22 -0800

    añadir predict.py

--- Caballos/predict.py
+++ Caballos/predict.py
@@ -4,5 +4,10 @@ from librerias import *
 
 def main():
     print(X_test)
+    def predict(model, df):
+    return model.predict(df.loc[:, ~df.columns.isin(['ID'])])
+
+
+    predictions = (X_test.groupby('ID').apply(lambda x: predict(clf_xgb, x)))
 
 main()
